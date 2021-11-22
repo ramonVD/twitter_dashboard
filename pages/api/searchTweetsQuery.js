87d4753f2c44ede734735292902935f61fa85318
Ramon <rvicentediaz@gmail.com>
2021-11-23 00:50:24 +0100

    Tweets are "responsive" now, fixed errors
    Fixed errors on last commit (unused vars, plugged the loading state into
    the login button, etc.)
    Moved some unused components and api stuff to an ignored folder, may
    need em in the future.
    
    Tweets are now loaded more sensibly, using a Twitter function on the
    window namespace. Code is less finicky, plus tweets are now kinda
    responsive: They will adapt to the page size when created, but they wont
    further resize because of iframe stuff (maybe solvable with pure css
    attributes but I need more time for this, didnt want to add an
    eventlistener).
    
    Reduced the amount of data that we get from the API, it gets everything
    we need this way I think.

--- pages/api/searchTweetsQuery.js
+++ pages/api/searchTweetsQuery.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
 
     //Desglossed url to be easier to change
     //Query with all possible data. Needed to get the full text from RTs?
+    /*
     const url = [
     `https://api.twitter.com/2/tweets/search/recent?query=${q}`,
     `&expansions=attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,`,
@@ -31,6 +32,14 @@ export default async function handler(req, res) {
     `&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type`,
     `&poll.fields=duration_minutes,end_datetime,id,options,voting_status`,
     `&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics,non_public_metrics,organic_metrics,promoted_metrics`,
+    `&max_results=${max_results}`].join("");*/
+
+    //Reduced the size of the search parameters, still tweaking
+    const url = [`https://api.twitter.com/2/tweets/search/recent?query=${q}`,
+    `&expansions=attachments.media_keys,author_id,entities.mentions.username`,
+    `&tweet.fields=author_id,conversation_id,created_at,entities,public_metrics,text,lang`,
+    `&user.fields=created_at,description,entities,id,location,name,profile_image_url,protected,public_metrics,url,username,verified`,
+    `&media.fields=media_key,url`,
     `&max_results=${max_results}`].join("");
 
     const myHeaders = {
